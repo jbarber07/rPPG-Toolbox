a14d445eb818bba8495dd874b25be2e9ed98d99d
jbarber07 <jerbarber@gmail.com>
2024-04-25 11:02:49 +0200

    num_workers fix + CHROME fix

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
                 
             data_loader_dict['train'] = DataLoader(
                 dataset=train_data_loader,
-                num_workers=16, 
+                num_workers=4, 
                 batch_size=config.TRAIN.BATCH_SIZE,
                 shuffle=True,
                 worker_init_fn=seed_worker,
@@ -201,7 +201,7 @@ if __name__ == "__main__":
                 config_data=config.VALID.DATA)
             data_loader_dict["valid"] = DataLoader(
                 dataset=valid_data,
-                num_workers=16,
+                num_workers=4,
                 batch_size=config.TRAIN.BATCH_SIZE,  # batch size for val is the same as train
                 shuffle=False,
                 worker_init_fn=seed_worker,
@@ -233,7 +233,7 @@ if __name__ == "__main__":
                 config_data=config.TEST.DATA)
             data_loader_dict["test"] = DataLoader(
                 dataset=test_data,
-                num_workers=16,
+                num_workers=4,
                 batch_size=config.INFERENCE.BATCH_SIZE,
                 shuffle=False,
                 worker_init_fn=seed_worker,
@@ -260,7 +260,7 @@ if __name__ == "__main__":
         
         data_loader_dict["unsupervised"] = DataLoader(
             dataset=unsupervised_data,
-            num_workers=16,
+            num_workers=4,
             batch_size=1,
             shuffle=False,
             worker_init_fn=seed_worker,
